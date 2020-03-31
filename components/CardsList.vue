272f2bdc5db92259ed7db75f6f8fd90b083b4244
MarianaHL <marianita_hernandez13@hotmail.com>
2020-03-31 15:02:34 -0600

    FEAT: Se agregaron componentes estructurales

---
+++ components/CardsList.vue
@@ -0,0 +1,14 @@
+<template>
+  <div>
+    <card-personajes />
+  </div>
+</template>
+
+<script>
+import CardPersonajes from '~/components/CardsPersonajes'
+export default {
+  components: {
+    CardPersonajes
+  }
+}
+</script>
---
+++ components/CardsPersonajes.vue
@@ -0,0 +1,18 @@
+<template>
+  <v-card
+    class="mx-auto"
+    max-width="400"
+  >
+    <v-img
+      class="white--text align-end"
+      height="200px"
+      src=""
+    >
+      <!-- /<v-card-title></v-card-title> -->
+    </v-img>
+
+    <v-card-title class="pb-0">
+      Nombre
+    </v-card-title>
+  </v-card>
+</template>
